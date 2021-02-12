931b4820805a45bcc37f244590f813de9393e033
hyska <hayriyeyasak1@gmail.com>
2021-02-12 21:46:09 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Eklendi");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Silindi");
+        }
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " Güncellendi");
+        }
+        public void GetAll(Musteri[] musteriler)
+        {
+            Console.WriteLine("~~~~Müşteri Listeleme~~~~" );
+            foreach (var musteri in musteriler) { 
+                Console.WriteLine("ID : " + musteri.Id);
+                Console.WriteLine("Tc : " + musteri.MusteriTc);
+                Console.WriteLine("Ad : " + musteri.MusteriAdi);
+                Console.WriteLine("Soyadı : " + musteri.MusteriSoyadi);
+                Console.WriteLine("Adres : " + musteri.MusteriAdres);
+                Console.WriteLine("~~~~~~~~~~");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.MusteriTc = "11111111111";
+            musteri1.MusteriAdi = "Ahmet";
+            musteri1.MusteriSoyadi = "Kale";
+            musteri1.MusteriAdres = "Efeler/Aydın";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.MusteriTc = "22222222222";
+            musteri2.MusteriAdi = "Mehmet";
+            musteri2.MusteriSoyadi = "Güzel";
+            musteri2.MusteriAdres = "Eyüp/İstanbul";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.MusteriTc = "33333333333";
+            musteri3.MusteriAdi = "Ayşe";
+            musteri3.MusteriSoyadi = "Yıldırım";
+            musteri3.MusteriAdres = "Fatih/İstanbul";
+
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            musteriManager.Add(musteri1);
+
+            musteriManager.Delete(musteri2);
+
+            musteriManager.Guncelle(musteri3);
+
+            musteriManager.GetAll(musteriler);
+        }
+
+        
+    }
+}
